4cb76d791b633e16d6aa1746a9dce302d62fd6cf
Pablodr89 <padoro89@gmail.com>
2023-09-12 13:48:18 +0200

    login,firbase,vuetify

--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
-import './assets/main.css'
-import 'vuetify/styles'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+//Vuetify
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+//Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
 
 import App from './App.vue'
 import router from './router'
 
+import './assets/main.css'
+
 const app = createApp(App)
+//Vuetify
 const vuetify = createVuetify({
     components,
     directives
+})
+
+//Firebase
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()]
 })
 
 app.use(createPinia())
---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import {  useForm, useField } from 'vee-validate'
+    import { loginSchema as validationSchema } from '../validation/loginSchema'
+
+    let { handleSubmit } = useForm({ validationSchema })
+
+    let email = useField('email')
+    let password = useField('password')
+
+    let submit = handleSubmit(() => {
+        console.log('validando')
+    })
+</script>
+
+<template>
+    <v-card flat max-width="600" class="mx-auto my-10">
+        <v-card-title class="text-h4 font-weight-bold" tag="h3">
+            Iniciar Sesión
+        </v-card-title>
+        <v-card-subtitle class="text-h5">
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5"> 
+            <v-text-field class="mb-3" v-model="email.value.value" type="email" label="Email" bg-color="blue-grey-lighten-5" :error-messages="email.errorMessage.value"/>
+            <v-text-field class="mb-3" v-model="password.value.value" type="password" label="Password" bg-color="blue-grey-lighten-5" :error-messages="password.errorMessage.value"/>
+
+            <v-btn block color="pink-accent-3" @click="submit">
+                Iniciar Sesion
+            </v-btn>
+        </v-form>
+    </v-card>
+</template>
